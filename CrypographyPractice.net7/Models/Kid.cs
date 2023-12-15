08fb04f6de24827200dac6b51b7667b32e9a127c
MegMoore <136514941+MegMoore@users.noreply.github.com>
2023-12-15 10:27:38 -0500

    Add project files.

---
+++ CrypographyPractice.net7/Models/Kid.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrypographyPractice.net7.Models
+{
+    [Index("Username", IsUnique = true)]
+    public class Kid
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Username { get; set; } = string.Empty;
+        [StringLength(50)]
+        public string HashPassword { get; set; } = string.Empty;
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+        
+        public DateTime DateOfBirth { get; set; }
+        public DateTime AdmissionDate { get; set; } = DateTime.Now;
+
+    }
+}
